2a6ec6536f660ccd23844522581669e6d21d3e3e
GremlenA <pedenkoartem5@gmail.com>
2026-01-05 11:27:53 +0200

    34

--- lib/api/api.tsx
+++ lib/api/api.tsx
@@ -1,6 +1,9 @@
 import axios from "axios";
 
+const baseURL =
+  process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3000";
+
 export const api = axios.create({
-  baseURL: "http://localhost:3000/api",
-  withCredentials: true, 
+  baseURL: `${baseURL}/api`,
+  withCredentials: true,
 });
